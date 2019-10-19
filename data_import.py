c9bdcbb7dd0ab5103285c6b70a054063904844c3
Desiree <desireemora@mail.usf.edu>
2019-10-18 22:55:06 -0500

    Create data_import.py
    data import

---
+++ data_import.py
@@ -0,0 +1,31 @@
+#Please download these libs by: Python -m pip install <package>
+import pandas
+import xlrd
+
+file_path = ("")
+
+#Workbook and worksheet setup
+wb = xlrd.open_workbook(file_path)
+sheet = wb.sheet_by_index(0) 
+
+#row,cell
+#row 0 =  column names
+sheet.cell_value(0,0)
+
+
+comp_pref = {}
+stud_pref = {}
+
+for r in range(sheet.nrows):
+    choices = []
+    choices.append(sheet.cell_value(r,1)
+    choices.append(sheet.cell_value(r,2)
+    choices.append(sheet.cell_value(r,3)
+    choices.append(sheet.cell_value(r,4)
+    choices.append(sheet.cell_value(r,5)
+                   
+    stud_pref[sheet.cell_value(r,0)] = 
+    sheet.cell_value(r,0)
+
+comp{"JP"} = stu
+
