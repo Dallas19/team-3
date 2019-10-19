dca9868447355d86d0b71605e8f4fd0e0ada7609
Desiree <desireemora@mail.usf.edu>
2019-10-19 01:05:31 -0500

    fix

--- data_import.py
+++ data_import.py
@@ -27,13 +27,13 @@ for r in range(sheet.nrows):
     if r == 0:
         continue
     choices = []
-    
+
     choices.append(sheet.cell_value(r,1))
     choices.append(sheet.cell_value(r,2))
     choices.append(sheet.cell_value(r,3))
     choices.append(sheet.cell_value(r,4))
     choices.append(sheet.cell_value(r,5))
-                   
+
     stud_pref[sheet.cell_value(r,0)] = choices
 
 companySlots = {}
@@ -47,9 +47,9 @@ for r in range(sheet2.nrows):
             continue
         if sheet2.cell_value(r,c) == "":
             break
-        
+
         choices.append(sheet2.cell_value(r,c))
-                       
+
     comp_pref[sheet2.cell_value(r,0)] = choices
     companySlots[sheet2.cell_value(r,0)] = 1
 
@@ -83,15 +83,15 @@ def matchmaker():
     for companyName in companys:
         if companyName not in matched:
              matched[companyName] = list()
-    studentRank2 = copy.deepcopy(studentRank)
-    # companyRank2 = copy.deepcopy(companyRank)
+    studentRank2 = copy.deepcopy(stud_pref)
+    # companyRank2 = copy.deepcopy(comp_pref)
     while unmatchedStudents:
         student = unmatchedStudents.pop(0)
         print("%s is on the market" % (student))
         studentRankList = studentRank2[student]
         print(studentRankList)
         company = studentRankList.pop(0)
-        print("  %s (company's #%s) is checking out %s (student's #%s)" % (student, (companyRank[company].index(student)+1), company, (studentRank[student].index(company)+1)) )
+        print("  %s (company's #%s) is checking out %s (student's #%s)" % (student, (comp_pref[company].index(student)+1), company, (comp_pref[student].index(company)+1)) )
         tempmatch = matched.get(company)
         print(tempmatch)
         if len(tempmatch) < companySlots.get(company):
@@ -100,21 +100,21 @@ def matchmaker():
                 print("    There's a spot! Now matched: %s and %s" % (student.upper(), company.upper()))
         else:
             # The student proposes to an full company!
-            companyslist = companyRank[company]
+            companyslist = comp_pref[company]
             for (i, matchedAlready) in enumerate(tempmatch):
                 if companyslist.index(matchedAlready) > companyslist.index(student):
                     # company prefers new student
                     if student not in matched[company]:
                         matched[company][i] = student
-                        print("  %s dumped %s (company's #%s) for %s (company's #%s)" % (company.upper(), matchedAlready, (companyRank[company].index(matchedAlready)+1), student.upper(), (companyRank[company].index(student)+1)))
-                        if studentRank[matchedAlready]:
+                        print("  %s dumped %s (company's #%s) for %s (company's #%s)" % (company.upper(), matchedAlready, (comp_pref[company].index(matchedAlready)+1), student.upper(), (comp_pref[company].index(student)+1)))
+                        if comp_pref[matchedAlready]:
                             # Ex has more companys to try
                             unmatchedStudents.append(matchedAlready)
                         else:
                             studentslost.append(matchedAlready)
                 else:
                     # company still prefers old match
-                    print("  %s would rather stay with %s (their #%s) over %s (their #%s)" % (company, matchedAlready, (companyRank[company].index(matchedAlready)+1), student, (companyRank[company].index(student)+1)))
+                    print("  %s would rather stay with %s (their #%s) over %s (their #%s)" % (company, matchedAlready, (comp_pref[company].index(matchedAlready)+1), student, (comp_pref[company].index(student)+1)))
                     if studentRankList:
                         # Look again
                         unmatchedStudents.append(student)
@@ -124,36 +124,36 @@ def matchmaker():
     for lostsoul in studentslost:
         print('%s did not match' % lostsoul)
     return (matched, studentslost)
- 
- 
- 
- 
- 
+
+
+
+
+
 def check(matched):
     inversematched = defaultdict(list)
     for companyName in matched.keys():
         for studentName in matched[companyName]:
             inversematched[companyName].append(studentName)
- 
+
     for companyName in matched.keys():
         for studentName in matched[companyName]:
- 
-            companyNamelikes = companyRank[companyName]
+
+            companyNamelikes = comp_pref[companyName]
             companyNamelikesbetter = companyNamelikes[:companyNamelikes.index(studentName)]
-            helikes = studentRank[studentName]
+            helikes = stud_pref[studentName]
             helikesbetter = helikes[:helikes.index(companyName)]
             for student in companyNamelikesbetter:
                 for p in inversematched.keys():
                     if student in inversematched[p]:
                         studentscompany = p
-                studentlikes = studentRank[student]
-                               
+                studentlikes = stud_pref[student]
+
                                 ## Not sure if this is correct
                 try:
                     studentlikes.index(studentscompany)
                 except ValueError:
                     continue
-                               
+
                 if studentlikes.index(studentscompany) > studentlikes.index(companyName):
                     print("%s and %s like each other better than "
                           "their present match: %s and %s, respectively"
@@ -161,7 +161,7 @@ def check(matched):
                     return False
             for company in helikesbetter:
                 companysstudents = matched[company]
-                companylikes = companyRank[company]
+                companylikes = comp_pref[company]
                 for companysstudent in companysstudents:
                     if companylikes.index(companysstudent) > companylikes.index(studentName):
                         print("%s and %s like each other better than "
@@ -169,18 +169,18 @@ def check(matched):
                               % (studentName, company, companyName, companysstudent))
                         return False
     return True
- 
- 
+
+
 print('\nPlay-by-play:')
 (matched, studentslost) = matchmaker()
- 
+
 print('\nCouples:')
 print('  ' + ',\n  '.join('%s is matched to %s' % couple
                           for couple in sorted(matched.items())))
 print
 print('Match stability check PASSED'
       if check(matched) else 'Match stability check FAILED')
- 
+
 print('\n\nSwapping two matches to introduce an error')
 matched[companys[0]], matched[companys[1]] = matched[companys[1]], matched[companys[0]]
 for company in companys[:2]:
@@ -188,4 +188,3 @@ for company in companys[:2]:
 print
 print('Match stability check PASSED'
       if check(matched) else 'Match stability check FAILED')
-
